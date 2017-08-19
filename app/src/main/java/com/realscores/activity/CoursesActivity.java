a17ae6145561c21390d351c68de0caa0c095698d
MitchB09 <mitchbilensky@gmail.com>
2017-08-19 13:30:36 -0300

    Refacter core activity setup code to its own class
    Moved base activity code for setup and nav drawer setup into its own
    reusable class

--- app/src/main/java/com/realscores/activity/CoursesActivity.java
+++ app/src/main/java/com/realscores/activity/CoursesActivity.java
@@ -1,51 +1,24 @@
 package com.realscores.activity;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 
-import com.realscores.activity.fragments.NavDrawerFragment;
-import com.realscores.activity.fragments.NavDrawerFragment.NavigationDrawerCallbacks;
 import com.realscores.R;
 import com.realscores.obj.Course;
 import com.realscores.service.CourseService;
 import com.realscores.service.ICourseService;
 
-public class CoursesActivity extends ActionBarActivity implements NavigationDrawerCallbacks {
+public class CoursesActivity extends BaseActivity {
 
   private ICourseService courseService = new CourseService(getBaseContext());
-
-  private NavDrawerFragment mNavigationDrawerFragment;
-
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.courses_activity);
-
-    mNavigationDrawerFragment = (NavDrawerFragment)
-        getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
-
-    mNavigationDrawerFragment.setUp(
-        R.id.navigation_drawer,
-        (DrawerLayout) findViewById(R.id.drawer_layout));
-  }
-
-  @Override
-  public void onNavigationDrawerItemSelected(int position) {
-    // update the main content by replacing fragments
-    FragmentManager fragmentManager = getSupportFragmentManager();
-    fragmentManager.beginTransaction()
-        .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
-        .commit();
+    super.setUpNavigationDrawer();
   }
 
   @Override
@@ -93,38 +66,4 @@ public class CoursesActivity extends ActionBarActivity implements NavigationDraw
       return super.onOptionsItemSelected(item);
     }
 
-
-  private void setCourseText(int courseId){
-
-  }
-
-  public static class PlaceholderFragment extends Fragment {
-    /**
-     * The fragment argument representing the section number for this
-     * fragment.
-     */
-    private static final String ARG_SECTION_NUMBER = "section_number";
-
-    public PlaceholderFragment() {
-    }
-
-    /**
-     * Returns a new instance of this fragment for the given section
-     * number.
-     */
-    public static PlaceholderFragment newInstance(int sectionNumber) {
-      PlaceholderFragment fragment = new PlaceholderFragment();
-      Bundle args = new Bundle();
-      args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-      fragment.setArguments(args);
-      return fragment;
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-      View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-      return rootView;
-    }
-  }
 }
